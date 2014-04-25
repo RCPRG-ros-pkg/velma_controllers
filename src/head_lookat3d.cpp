d151ca6a2d3b95f9be5c1440dab911ad057cde06
mwalecki <mw@mwalecki.pl>
2014-04-25 14:51:29 +0200

    style fix

--- src/head_lookat3d.cpp
+++ src/head_lookat3d.cpp
@@ -1,93 +1,99 @@
+// Copyright 2014 WUT
+/*
+ * head_lookat3d.cpp.cpp
+ *
+ *  Created on: 28 mar 2014
+ *      Author: mwalecki
+ */
+
 #include <rtt/TaskContext.hpp>
 #include <rtt/Port.hpp>
 #include <rtt/Component.hpp>
 
-#include <vector>
-#include <Eigen/Dense>
+#include "head_kinematics.h"
 
 #include <geometry_msgs/Pose.h>
 
-#include "head_kinematics.h"
+#include <Eigen/Dense>
+#include <string>
+#include <vector>
 
-#define H_ROT	0.687d
-#define H_LEAN	0.375d
-#define H_HEAD	0.385d
-#define H_CAM	0.200d
+#define H_ROT  0.687d
+#define H_LEAN  0.375d
+#define H_HEAD  0.385d
+#define H_CAM  0.200d
 
 #define HEAD_CTRL_MNJ 2
 #define FULL_TORSO_MNJ 4
-#define DEFAULT_TARGET_X	1.00d
-#define DEFAULT_TARGET_Y	0.00d
-#define DEFAULT_TARGET_Z	1.54d
+#define DEFAULT_TARGET_X  1.00d
+#define DEFAULT_TARGET_Y  0.00d
+#define DEFAULT_TARGET_Z  1.54d
 
 class HeadLookAt3D : public RTT::TaskContext {
-public:
-	HeadLookAt3D(const std::string & name) : TaskContext(name, PreOperational) {
+ public:
+  explicit HeadLookAt3D(const std::string & name) : TaskContext(name, PreOperational) {
+    this->ports()->addPort("HeadJointPositionCommand", port_HeadJointPositionCommand).doc("");
+    this->ports()->addPort("TargetPoint", port_TargetPoint).doc("");
+    this->ports()->addPort("JointPosition", port_JointPosition).doc("");
+  }
 
-		this->ports()->addPort("HeadJointPositionCommand", port_HeadJointPositionCommand).doc("");
-		this->ports()->addPort("TargetPoint", port_TargetPoint).doc("");
-		this->ports()->addPort("JointPosition", port_JointPosition).doc("");
-	}
+  ~HeadLookAt3D() {
+  }
 
-	~HeadLookAt3D() {
-	}
+  bool configureHook() {
+    head_jnt_pos_cmd_.resize(HEAD_CTRL_MNJ);
+    port_HeadJointPositionCommand.setDataSample(head_jnt_pos_cmd_);
+    jnt_pos_.resize(FULL_TORSO_MNJ);
 
-	bool configureHook() {
-		head_jnt_pos_cmd_.resize(HEAD_CTRL_MNJ);
-		port_HeadJointPositionCommand.setDataSample(head_jnt_pos_cmd_);
-		jnt_pos_.resize(FULL_TORSO_MNJ);
-		
-		h = new HeadKinematics(H_ROT, H_LEAN, H_HEAD, H_CAM);
-		
-		current_point.position.x = DEFAULT_TARGET_X;
-		current_point.position.y = DEFAULT_TARGET_Y;
-		current_point.position.z = DEFAULT_TARGET_Z;
+    h = new HeadKinematics(H_ROT, H_LEAN, H_HEAD, H_CAM);
 
-		return true;
-	}
+    current_point.position.x = DEFAULT_TARGET_X;
+    current_point.position.y = DEFAULT_TARGET_Y;
+    current_point.position.z = DEFAULT_TARGET_Z;
 
-	bool startHook() {
-		
-		return true;
-	}
+    return true;
+  }
 
-	void stopHook() {
-	}
+  bool startHook() {
+    return true;
+  }
 
-	void updateHook() {
-		double joint_pan, joint_tilt;
-		RTT::FlowStatus target_point_fs, jnt_pos_fs;
-		
-		target_point_fs = port_TargetPoint.read(target_point);
-		if(target_point_fs == RTT::NewData){
-			current_point.position = target_point.position;
-		}
-		
-		jnt_pos_fs = port_JointPosition.read(jnt_pos_);
-		if(jnt_pos_fs == RTT::NewData){
-			h->UpdateTorsoPose(jnt_pos_(0), jnt_pos_(1));
-			h->UpdateTargetPosition(current_point.position.x, current_point.position.y, current_point.position.z);
-			h->TransformTargetToHeadFrame();
-			if(h->CalculateHeadPose(joint_pan, joint_tilt) == 0){
-				head_jnt_pos_cmd_(0) = joint_pan;
-				head_jnt_pos_cmd_(1) = joint_tilt;
-			}
-			port_HeadJointPositionCommand.write(head_jnt_pos_cmd_);
-		}
-	}
+  void stopHook() {
+  }
 
-private:
-	HeadKinematics *h;
+  void updateHook() {
+    double joint_pan, joint_tilt;
+    RTT::FlowStatus target_point_fs, jnt_pos_fs;
+    
+    target_point_fs = port_TargetPoint.read(target_point);
+    if(target_point_fs == RTT::NewData) {
+      current_point.position = target_point.position;
+    }
 
-	RTT::OutputPort<Eigen::VectorXd > port_HeadJointPositionCommand;
-	RTT::InputPort<geometry_msgs::Pose > port_TargetPoint;
-	RTT::InputPort<Eigen::VectorXd > port_JointPosition;
-	
-	geometry_msgs::Pose target_point;
-	geometry_msgs::Pose current_point;
-	Eigen::VectorXd head_jnt_pos_cmd_;
-	Eigen::VectorXd jnt_pos_;
+    jnt_pos_fs = port_JointPosition.read(jnt_pos_);
+    if(jnt_pos_fs == RTT::NewData) {
+      h->UpdateTorsoPose(jnt_pos_(0), jnt_pos_(1));
+      h->UpdateTargetPosition(current_point.position.x, current_point.position.y, current_point.position.z);
+      h->TransformTargetToHeadFrame();
+      if(h->CalculateHeadPose(joint_pan, joint_tilt) == 0) {
+        head_jnt_pos_cmd_(0) = joint_pan;
+        head_jnt_pos_cmd_(1) = joint_tilt;
+      }
+      port_HeadJointPositionCommand.write(head_jnt_pos_cmd_);
+    }
+  }
 
+ private:
+  HeadKinematics *h;
+
+  RTT::OutputPort<Eigen::VectorXd > port_HeadJointPositionCommand;
+  RTT::InputPort<geometry_msgs::Pose > port_TargetPoint;
+  RTT::InputPort<Eigen::VectorXd > port_JointPosition;
+
+  geometry_msgs::Pose target_point;
+  geometry_msgs::Pose current_point;
+  Eigen::VectorXd head_jnt_pos_cmd_;
+  Eigen::VectorXd jnt_pos_;
 };
 
 ORO_CREATE_COMPONENT(HeadLookAt3D)
