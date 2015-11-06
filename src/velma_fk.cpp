6e02a5810aceb12fa84d2b8b0f76b7d92ebb46ee
Dawid Seredyński <dawid.seredynski@gmail.com>
2015-11-06 16:50:53 +0100

    fixed tool exchange bug

--- src/velma_fk.cpp
+++ src/velma_fk.cpp
@@ -45,25 +45,25 @@ void VelmaFK::updateHook() {
   port_joint_position_command_.read(joint_position_command_);
 
   if (port_left_tool_position_command_.read(tool_left) == RTT::NewData) {
-    tools_[0](0) = tool_left.position.x;
-    tools_[0](1) = tool_left.position.y;
-    tools_[0](2) = tool_left.position.z;
+    tools_[1](0) = tool_left.position.x;
+    tools_[1](1) = tool_left.position.y;
+    tools_[1](2) = tool_left.position.z;
 
-    tools_[0](3) = tool_left.orientation.w;
-    tools_[0](4) = tool_left.orientation.x;
-    tools_[0](5) = tool_left.orientation.y;
-    tools_[0](6) = tool_left.orientation.z;
+    tools_[1](3) = tool_left.orientation.w;
+    tools_[1](4) = tool_left.orientation.x;
+    tools_[1](5) = tool_left.orientation.y;
+    tools_[1](6) = tool_left.orientation.z;
   }
 
   if (port_right_tool_position_command_.read(tool_right) == RTT::NewData) {
-    tools_[1](0) = tool_right.position.x;
-    tools_[1](1) = tool_right.position.y;
-    tools_[1](2) = tool_right.position.z;
+    tools_[0](0) = tool_right.position.x;
+    tools_[0](1) = tool_right.position.y;
+    tools_[0](2) = tool_right.position.z;
 
-    tools_[1](3) = tool_right.orientation.w;
-    tools_[1](4) = tool_right.orientation.x;
-    tools_[1](5) = tool_right.orientation.y;
-    tools_[1](6) = tool_right.orientation.z;
+    tools_[0](3) = tool_right.orientation.w;
+    tools_[0](4) = tool_right.orientation.x;
+    tools_[0](5) = tool_right.orientation.y;
+    tools_[0](6) = tool_right.orientation.z;
   }
 
   robot_->fkin(r, joint_position_command_, &tools_[0]);
