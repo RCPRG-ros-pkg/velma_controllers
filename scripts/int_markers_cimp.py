72e3989e61426fc5b067e21ffea83c7cbb404841
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-02-03 12:06:30 +0100

    both arms support, code cleanup

--- scripts/int_markers_cimp.py
+++ scripts/int_markers_cimp.py
@@ -18,266 +18,229 @@ import PyKDL
 from cartesian_trajectory_msgs.msg import *
 import actionlib
 
-def getTransformations():
-    global listener
-    global prefix
-    global T_B_W
-    global T_W_T
-    global T_T_W
+class IntMarkersCimp:
+    def __init__(self, prefix):
+        self.prefix = prefix
 
-    pose = listener.lookupTransform('torso_base', prefix+'_arm_7_link', rospy.Time(0))
-    T_B_W = pm.fromTf(pose)
+        self.action_trajectory_client = None
 
-    pose_tool = listener.lookupTransform(prefix+'_arm_7_link', prefix+'_arm_tool', rospy.Time(0))
-    T_W_T = pm.fromTf(pose_tool)
-    T_T_W = T_W_T.Inverse()
+        self.T_B_W = None
+        self.T_W_T = None
+        self.T_T_W = None
 
-def erase6DofMarker():
-    global server
-    server.erase(prefix+'_arm_position_marker')
-    server.applyChanges();
+        self.listener = tf.TransformListener();
 
-def insert6DofGlobalMarker():
-    global server
-    global T_B_W
-    global T_W_T
+        # create an interactive marker server on the topic namespace simple_marker
+        self.server = InteractiveMarkerServer('int_markers_cimp_' + self.prefix)
 
-    T_B_T = T_B_W * T_W_T
-    int_position_marker = InteractiveMarker()
-    int_position_marker.header.frame_id = 'torso_base'
-    int_position_marker.name = prefix+'_arm_position_marker'
-    int_position_marker.scale = 0.2
-    int_position_marker.pose = pm.toMsg(T_B_T)
+        while not rospy.is_shutdown():
+            rospy.sleep(1)
+            time_now = rospy.Time.now() - rospy.Duration(0.5)
+            try:
+                self.listener.waitForTransform('torso_base', self.prefix+'_arm_7_link', time_now, rospy.Duration(60.0))
+                self.listener.waitForTransform(self.prefix+'_arm_7_link', self.prefix+'_arm_tool', time_now, rospy.Duration(60.0))
+                break
+            except:
+                print "int_markers_cimp_[%s] script: waitForTransform error, retrying in 1s..."%(self.prefix)
 
-    int_position_marker.controls.append(createInteractiveMarkerControl6DOF(InteractiveMarkerControl.ROTATE_AXIS,'x'));
-    int_position_marker.controls.append(createInteractiveMarkerControl6DOF(InteractiveMarkerControl.ROTATE_AXIS,'y'));
-    int_position_marker.controls.append(createInteractiveMarkerControl6DOF(InteractiveMarkerControl.ROTATE_AXIS,'z'));
-    int_position_marker.controls.append(createInteractiveMarkerControl6DOF(InteractiveMarkerControl.MOVE_AXIS,'x'));
-    int_position_marker.controls.append(createInteractiveMarkerControl6DOF(InteractiveMarkerControl.MOVE_AXIS,'y'));
-    int_position_marker.controls.append(createInteractiveMarkerControl6DOF(InteractiveMarkerControl.MOVE_AXIS,'z'));
+        if rospy.is_shutdown():
+            exit(0)
 
-    box = createAxisMarkerControl(Point(0.15,0.015,0.015), Point(0.0, 0.0, 0.0) )
-    box.interaction_mode = InteractiveMarkerControl.BUTTON
-    box.name = 'button'
-    int_position_marker.controls.append( box )
-    server.insert(int_position_marker, processFeedback);
-    server.applyChanges();
+        self.getTransformations()
 
-def globalState():
-    global marker_state
-    global prefix
+        self.insert6DofGlobalMarker()
 
-    if marker_state == "global" or marker_state=="local":
-       erase6DofMarker()
-
-    marker_state="global"
-    insert6DofGlobalMarker()
-
-def hiddenState():
-    global marker_state
-
-    if marker_state == "global" or marker_state=="local":
-       erase6DofMarker()
-
-    marker_state="hidden"
-
-def processFeedbackHide(feedback):
-    global marker_state
-
-    if ( feedback.event_type == InteractiveMarkerFeedback.BUTTON_CLICK and feedback.control_name == "button_hide" ):
-       if marker_state == "global":
-           hiddenState()
-       else:
-           globalState();
-
-def processFeedback(feedback):
-
-    global prefix
-    global hand_arm_transform
-    global tool
-    global p
-    global action_trajectory_client
-
-    if action_trajectory_client == None:
-        return
-
-    print "feedback", feedback.marker_name, feedback.control_name, feedback.event_type
-
-    if ( feedback.marker_name == prefix+'_arm_position_marker' ) and ( feedback.event_type == InteractiveMarkerFeedback.BUTTON_CLICK ) and ( feedback.control_name == "button" ):
-        T_B_Td = pm.fromMsg(feedback.pose)
-        p = pm.toMsg(T_B_Td)
-        duration = 5.0
-
-        action_trajectory_goal = CartesianTrajectoryGoal()
+        self.marker_state = "global"
+        self.int_hide_marker = InteractiveMarker()
+        self.int_hide_marker.header.frame_id = 'torso_base'
+        self.int_hide_marker.name = self.prefix+'_arm_hide_marker';
+        self.int_hide_marker.scale = 0.2
+        self.int_hide_marker.pose.orientation = Quaternion(0,0,0,1)
+        if self.prefix == "right":
+            self.int_hide_marker.pose.position = Point(0, -0.5, 0)
+            self.hide = self.createButtoMarkerControl(Point(0.15,0.15,0.15), Point(0.0, 0.0, 0.0), ColorRGBA(1,0,0,1) )
+        else:
+            self.int_hide_marker.pose.position = Point(0, 0.5, 0)
+            self.hide = self.createButtoMarkerControl(Point(0.15,0.15,0.15), Point(0.0, 0.0, 0.0), ColorRGBA(0,1,0,1) )
+        self.hide.interaction_mode = InteractiveMarkerControl.BUTTON
+        self.hide.name = 'button_hide'
+        self.int_hide_marker.controls.append( self.hide )
+        self.server.insert(self.int_hide_marker, self.processFeedbackHide);
         
-        action_trajectory_goal.trajectory.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
-        
-        action_trajectory_goal.trajectory.points.append(CartesianTrajectoryPoint(
-        rospy.Duration(duration),
-        p,
-        Twist()))
+        self.server.applyChanges();
 
-        action_trajectory_client.send_goal(action_trajectory_goal)
+        self.action_trajectory_client = actionlib.SimpleActionClient("/" + self.prefix + "_arm/cartesian_trajectory", CartesianTrajectoryAction)
+        print "int_markers_cimp_[%s] script: waiting for /[%s]_arm/cartesian_trajectory action..."%(self.prefix, self.prefix)
+        self.action_trajectory_client.wait_for_server()
+        print "int_markers_cimp_[%s] script: connected to action /[%s]_arm/cartesian_trajectory"%(self.prefix, self.prefix)
 
-        print "duration: %s"%(duration)
-        print "pose: %s"%(p)
+    def getTransformations(self):
+        pose = self.listener.lookupTransform('torso_base', self.prefix+'_arm_7_link', rospy.Time(0))
+        self.T_B_W = pm.fromTf(pose)
 
-def createSphereMarkerControl(scale, position, color):
-    marker = Marker()
-    marker.type = Marker.SPHERE
-    marker.scale = scale
-    marker.pose.position = position
-    marker.color = color
-    control = InteractiveMarkerControl()
-    control.always_visible = True;
-    control.markers.append( marker );
-    return control
+        pose_tool = self.listener.lookupTransform(self.prefix+'_arm_7_link', self.prefix+'_arm_tool', rospy.Time(0))
+        self.T_W_T = pm.fromTf(pose_tool)
+        self.T_T_W = self.T_W_T.Inverse()
 
-def createBoxMarkerControl(scale, position):
-    marker = Marker()
-    marker.type = Marker.CUBE
-    marker.scale = scale
-    marker.pose.position = position
-    marker.color = ColorRGBA(0.5,0.5,0.5,1)
-    control = InteractiveMarkerControl()
-    control.always_visible = True;
-    control.markers.append( marker );
-    return control
+    def erase6DofMarker(self):
+        self.server.erase(self.prefix+'_arm_position_marker')
+        self.server.applyChanges();
 
-def createAxisMarkerControl(scale, position):
-    markerX = Marker()
-    markerX.type = Marker.ARROW
-    markerX.scale = scale
-    markerX.pose.position = position
-    ori = quaternion_about_axis(0, [0, 1 ,0])
-    markerX.pose.orientation = Quaternion(ori[0], ori[1], ori[2], ori[3])
-    markerX.color = ColorRGBA(1,0,0,1)
-    markerY = Marker()
-    markerY.type = Marker.ARROW
-    markerY.scale = scale
-    markerY.pose.position = position
-    ori = quaternion_about_axis(math.pi/2.0, [0, 0 ,1])
-    markerY.pose.orientation = Quaternion(ori[0], ori[1], ori[2], ori[3])
-    markerY.color = ColorRGBA(0,1,0,1)
-    markerZ = Marker()
-    markerZ.type = Marker.ARROW
-    markerZ.scale = scale
-    markerZ.pose.position = position
-    ori = quaternion_about_axis(-math.pi/2.0, [0, 1 ,0])
-    markerZ.pose.orientation = Quaternion(ori[0], ori[1], ori[2], ori[3])
-    markerZ.color = ColorRGBA(0,0,1,1)
-    control = InteractiveMarkerControl()
-    control.always_visible = True;
-    control.markers.append( markerX );
-    control.markers.append( markerY );
-    control.markers.append( markerZ );
-    return control
+    def insert6DofGlobalMarker(self):
+        T_B_T = self.T_B_W * self.T_W_T
+        int_position_marker = InteractiveMarker()
+        int_position_marker.header.frame_id = 'torso_base'
+        int_position_marker.name = self.prefix+'_arm_position_marker'
+        int_position_marker.scale = 0.2
+        int_position_marker.pose = pm.toMsg(T_B_T)
 
-def createButtoMarkerControl(scale, position, color):
-    marker = Marker()
-    marker.type = Marker.SPHERE
-    marker.scale = scale
-    marker.pose.position = position
-    marker.pose.orientation = Quaternion(0,0,0,1)
-    marker.color = color
-    control = InteractiveMarkerControl()
-    control.always_visible = True;
-    control.markers.append( marker );
-    return control
+        int_position_marker.controls.append(self.createInteractiveMarkerControl6DOF(InteractiveMarkerControl.ROTATE_AXIS,'x'));
+        int_position_marker.controls.append(self.createInteractiveMarkerControl6DOF(InteractiveMarkerControl.ROTATE_AXIS,'y'));
+        int_position_marker.controls.append(self.createInteractiveMarkerControl6DOF(InteractiveMarkerControl.ROTATE_AXIS,'z'));
+        int_position_marker.controls.append(self.createInteractiveMarkerControl6DOF(InteractiveMarkerControl.MOVE_AXIS,'x'));
+        int_position_marker.controls.append(self.createInteractiveMarkerControl6DOF(InteractiveMarkerControl.MOVE_AXIS,'y'));
+        int_position_marker.controls.append(self.createInteractiveMarkerControl6DOF(InteractiveMarkerControl.MOVE_AXIS,'z'));
 
-def createInteractiveMarkerControl6DOF(mode, axis):
-    control = InteractiveMarkerControl()
-    control.orientation_mode = InteractiveMarkerControl.FIXED
-    if mode == InteractiveMarkerControl.ROTATE_AXIS:
-        control.name = 'rotate_';
-    if mode == InteractiveMarkerControl.MOVE_AXIS:
-        control.name = 'move_';
-    if axis == 'x':
-        control.orientation = Quaternion(1,0,0,1)
-        control.name = control.name+'x';
-    if axis == 'y':
-        control.orientation = Quaternion(0,1,0,1)
-        control.name = control.name+'x';
-    if axis == 'z':
-        control.orientation = Quaternion(0,0,1,1)
-        control.name = control.name+'x';
-    control.interaction_mode = mode
-    return control
+        box = self.createAxisMarkerControl(Point(0.15,0.015,0.015), Point(0.0, 0.0, 0.0) )
+        box.interaction_mode = InteractiveMarkerControl.BUTTON
+        box.name = 'button'
+        int_position_marker.controls.append( box )
+        self.server.insert(int_position_marker, self.processFeedback);
+        self.server.applyChanges();
+
+    def globalState(self):
+        if self.marker_state == "global" or self.marker_state=="local":
+           self.erase6DofMarker()
+
+        self.marker_state = "global"
+        self.insert6DofGlobalMarker()
+
+    def hiddenState(self):
+        if self.marker_state == "global" or self.marker_state=="local":
+           self.erase6DofMarker()
+
+        self.marker_state="hidden"
+
+    def processFeedbackHide(self, feedback):
+        if ( feedback.event_type == InteractiveMarkerFeedback.BUTTON_CLICK and feedback.control_name == "button_hide" ):
+           if self.marker_state == "global":
+               self.hiddenState()
+           else:
+               self.globalState();
+
+    def processFeedback(self, feedback):
+        if self.action_trajectory_client == None:
+            return
+
+        print "feedback", feedback.marker_name, feedback.control_name, feedback.event_type
+
+        if ( feedback.marker_name == self.prefix+'_arm_position_marker' ) and ( feedback.event_type == InteractiveMarkerFeedback.BUTTON_CLICK ) and ( feedback.control_name == "button" ):
+            T_B_Td = pm.fromMsg(feedback.pose)
+            self.p = pm.toMsg(T_B_Td)
+            duration = 5.0
+
+            action_trajectory_goal = CartesianTrajectoryGoal()
+            
+            action_trajectory_goal.trajectory.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
+            
+            action_trajectory_goal.trajectory.points.append(CartesianTrajectoryPoint(
+            rospy.Duration(duration),
+            self.p,
+            Twist()))
+
+            self.action_trajectory_client.send_goal(action_trajectory_goal)
+
+            print "duration: %s"%(duration)
+            print "pose: %s"%(self.p)
+
+    def createSphereMarkerControl(self, scale, position, color):
+        marker = Marker()
+        marker.type = Marker.SPHERE
+        marker.scale = scale
+        marker.pose.position = position
+        marker.color = color
+        control = InteractiveMarkerControl()
+        control.always_visible = True;
+        control.markers.append( marker );
+        return control
+
+    def createBoxMarkerControl(self, scale, position):
+        marker = Marker()
+        marker.type = Marker.CUBE
+        marker.scale = scale
+        marker.pose.position = position
+        marker.color = ColorRGBA(0.5,0.5,0.5,1)
+        control = InteractiveMarkerControl()
+        control.always_visible = True;
+        control.markers.append( marker );
+        return control
+
+    def createAxisMarkerControl(self, scale, position):
+        markerX = Marker()
+        markerX.type = Marker.ARROW
+        markerX.scale = scale
+        markerX.pose.position = position
+        ori = quaternion_about_axis(0, [0, 1 ,0])
+        markerX.pose.orientation = Quaternion(ori[0], ori[1], ori[2], ori[3])
+        markerX.color = ColorRGBA(1,0,0,1)
+        markerY = Marker()
+        markerY.type = Marker.ARROW
+        markerY.scale = scale
+        markerY.pose.position = position
+        ori = quaternion_about_axis(math.pi/2.0, [0, 0 ,1])
+        markerY.pose.orientation = Quaternion(ori[0], ori[1], ori[2], ori[3])
+        markerY.color = ColorRGBA(0,1,0,1)
+        markerZ = Marker()
+        markerZ.type = Marker.ARROW
+        markerZ.scale = scale
+        markerZ.pose.position = position
+        ori = quaternion_about_axis(-math.pi/2.0, [0, 1 ,0])
+        markerZ.pose.orientation = Quaternion(ori[0], ori[1], ori[2], ori[3])
+        markerZ.color = ColorRGBA(0,0,1,1)
+        control = InteractiveMarkerControl()
+        control.always_visible = True;
+        control.markers.append( markerX );
+        control.markers.append( markerY );
+        control.markers.append( markerZ );
+        return control
+
+    def createButtoMarkerControl(self, scale, position, color):
+        marker = Marker()
+        marker.type = Marker.SPHERE
+        marker.scale = scale
+        marker.pose.position = position
+        marker.pose.orientation = Quaternion(0,0,0,1)
+        marker.color = color
+        control = InteractiveMarkerControl()
+        control.always_visible = True;
+        control.markers.append( marker );
+        return control
+
+    def createInteractiveMarkerControl6DOF(self, mode, axis):
+        control = InteractiveMarkerControl()
+        control.orientation_mode = InteractiveMarkerControl.FIXED
+        if mode == InteractiveMarkerControl.ROTATE_AXIS:
+            control.name = 'rotate_';
+        if mode == InteractiveMarkerControl.MOVE_AXIS:
+            control.name = 'move_';
+        if axis == 'x':
+            control.orientation = Quaternion(1,0,0,1)
+            control.name = control.name+'x';
+        if axis == 'y':
+            control.orientation = Quaternion(0,1,0,1)
+            control.name = control.name+'x';
+        if axis == 'z':
+            control.orientation = Quaternion(0,0,1,1)
+            control.name = control.name+'x';
+        control.interaction_mode = mode
+        return control
 
 if __name__ == "__main__":
-    a = []
-    for arg in sys.argv:
-        a.append(arg)
 
-    if len(a) < 2:
-        print "Usage: %s prefix"%a[0]
-        print "you provided:"
-        for x in a:
-            print x
-        exit(0)
+    rospy.init_node('int_markers_cimp', anonymous=False)
 
-    prefix = a[1]
-    if prefix != "left" and prefix != "right":
-        print "Usage: %s prefix"%a[0]
-        print "prefix should be left or right"
-        print "you provided:"
-        for x in a:
-            print x
-        exit(0)
-
-    rospy.init_node(prefix+'_arm_markers', anonymous=True)
-
-    action_trajectory_client = None
-
-    T_B_W = None
-    T_W_T = None
-    T_T_W = None
-
-    listener = tf.TransformListener();
-
-    # create an interactive marker server on the topic namespace simple_marker
-    server = InteractiveMarkerServer(prefix+'_arm_markers')
-
-    while not rospy.is_shutdown():
-        rospy.sleep(5)
-        time_now = rospy.Time.now() - rospy.Duration(0.5)
-        try:
-            listener.waitForTransform('torso_base', prefix+'_arm_7_link', time_now, rospy.Duration(60.0))
-            listener.waitForTransform(prefix+'_arm_7_link', prefix+'_arm_tool', time_now, rospy.Duration(60.0))
-            break
-        except:
-            print "arm_markers [%s] script: waitForTransform error, retrying in 5s..."%(prefix)
-
-    if rospy.is_shutdown():
-        exit(0)
-
-    getTransformations()
-
-    insert6DofGlobalMarker()
-
-    marker_state="global"
-    int_hide_marker = InteractiveMarker()
-    int_hide_marker.header.frame_id = 'torso_base'
-    int_hide_marker.name = prefix+'_arm_hide_marker';
-    int_hide_marker.scale = 0.2
-    int_hide_marker.pose.orientation = Quaternion(0,0,0,1)
-    if prefix == "right":
-        int_hide_marker.pose.position = Point(0, -0.5, 0)
-        hide = createButtoMarkerControl(Point(0.15,0.15,0.15), Point(0.0, 0.0, 0.0), ColorRGBA(1,0,0,1) )
-    else:
-        int_hide_marker.pose.position = Point(0, 0.5, 0)
-        hide = createButtoMarkerControl(Point(0.15,0.15,0.15), Point(0.0, 0.0, 0.0), ColorRGBA(0,1,0,1) )
-    hide.interaction_mode = InteractiveMarkerControl.BUTTON
-    hide.name = 'button_hide'
-    int_hide_marker.controls.append( hide )
-    server.insert(int_hide_marker, processFeedbackHide);
-    
-    server.applyChanges();
-
-    pub = rospy.Publisher('/' + prefix + '_arm/tool_marker', Marker, queue_size=100)
-
-    action_trajectory_client = actionlib.SimpleActionClient("/" + prefix + "_arm/cartesian_trajectory", CartesianTrajectoryAction)
-    action_trajectory_client.wait_for_server()
+    int_right = IntMarkersCimp("right")
+    int_left = IntMarkersCimp("left")
 
     rospy.spin()
 
